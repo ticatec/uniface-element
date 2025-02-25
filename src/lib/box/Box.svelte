e302f1cf5e907f12937af04d0f40320a589d698a
henryf <fenghl@anmed.com.cn>
2025-02-25 10:10:22 +0800

    add content$style to update content element style.

--- src/lib/box/Box.svelte
+++ src/lib/box/Box.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
 
     export let style: string = "";
-    
+    export let title: string = null as unknown as string;
+    export let round: boolean = false;
+
+    export let content$style: string = '';
+
 </script>
 
-<div {style} class="uniface-box">
-    <slot></slot>
+<div {style} class="uniface-box" class:round>
+    {#if title || $$slots['header']}
+        <div class="box-title">
+            <slot name="header">
+                <div style="width: 100%; box-sizing: border-box; padding: 6px 12px; overflow: hidden; text-overflow: ellipsis">
+                    <span>{title}</span>
+                </div>
+            </slot>
+        </div>
+    {/if}
+    <div class="box-content" style={content$style}>
+        <slot></slot>
+    </div>
 </div>
