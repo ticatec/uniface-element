7c877df444ada35d7a41793fb90e07a91aac640f
henryf <fenghl@anmed.com.cn>
2025-03-10 06:03:05 +0800

    add concise list table

---
+++ src/lib/concise-data-table/index.ts
@@ -0,0 +1,7 @@
+import ConciseListTable from "$lib/concise-data-table/ConciseListTable.svelte";
+import type Column from "$lib/concise-data-table/Column";
+import type {TableOptions} from "$lib/concise-data-table/TableOptions";
+
+export default ConciseListTable;
+
+export type {Column, TableOptions}
